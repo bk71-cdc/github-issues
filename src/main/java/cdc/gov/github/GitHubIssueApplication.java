cee593c896d4191edfc8e73947c979817cf968a6
BintabenPatel <BK71@cdc.gov>
2026-05-08 10:26:12 -0400

    Source code

---
+++ src/main/java/cdc/gov/github/GitHubIssueApplication.java
@@ -0,0 +1,11 @@
+package cdc.gov.github;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubIssueApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GitHubIssueApplication.class, args);
+    }
+}
---
+++ src/main/java/cdc/gov/github/service/TicketService.java
@@ -0,0 +1,9 @@
+package cdc.gov.github.service;
+
+import cdc.gov.github.dto.LoadTicketsResponse;
+
+public interface TicketService {
+    LoadTicketsResponse loadTicketsFromExcel(String fileName);
+    LoadTicketsResponse createEmptyIssues(int numberOfIssues);
+    LoadTicketsResponse updateTicketsFromExcel(String fileName);
+}
